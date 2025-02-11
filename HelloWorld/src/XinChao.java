bfaab5b7af3db67bd5283291a57d1791ffc34fb0
NguyenQuangDat123 <dat.nq.63cntt@ntu.edu.vn>
2025-02-11 10:47:14 +0700

    Cập nhật code
    sửa mới và thêm dòng

--- HelloWorld/src/XinChao.java
+++ HelloWorld/src/XinChao.java
@@ -12,8 +12,8 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// 
-		System.out.printf("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quản lý mã nguồn với GIT");
-		
+		System.out.printf("TÔI LẦN ĐẦU HỌC Java, và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.printf("Tôi thêm nội dung mới, và sửa lại chữ Java ở trên");
 	}
 
 }
