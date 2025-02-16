d3cc174c69ce11200d5a554857b12faadb2b9f57
NguyenQuangDat123 <dat.nq.63cntt@ntu.edu.vn>
2025-02-16 18:29:06 +0700

    Bài 4: Chương trình quản lý sinh viên
    Lỗi class

---
+++ Lab7tt/src/Bai4_QLSV/SinhVienPoly.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Bai4_QLSV;
+
+/**
+ * 
+ */
+public class SinhVienPoly {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
