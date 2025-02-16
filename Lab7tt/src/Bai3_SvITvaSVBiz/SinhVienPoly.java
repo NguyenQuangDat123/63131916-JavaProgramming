fd3d88bfaa7e3c0c5cfd468ac9f8ef6ac2e1517c
NguyenQuangDat123 <dat.nq.63cntt@ntu.edu.vn>
2025-02-16 18:25:01 +0700

    Bài 3: Sinh viên IT và Sinh viên Biz
    Thành công

---
+++ Lab7tt/src/Bai3_SvITvaSVBiz/SinhVienPoly.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Bai3_SvITvaSVBiz;
+
+/**
+ * 
+ */
+public class SinhVienPoly {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
